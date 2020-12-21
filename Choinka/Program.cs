b0ac231957f37dafa70517b9823a653f2e5424e1
sebnorth <sebnorth@gmail.com>
2020-12-21 15:57:20 +0100

    Dodaj pliki projektów.

---
+++ Choinka/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Choinka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int p = 2 * n - 1;
+            int x, k;
+
+            for (int i = 1; i <= n; i++) // wiersze
+            {
+
+                x = 2 * i - 1; // ilość gwiazdek
+                // spacje
+                for (k = 0; k < (p-x)/2; k++)
+                {
+                    Console.Write(" ");
+                }
+
+                // krzyżyki 
+                for (k = 0; k < x; k++)
+                {
+                    Console.Write("*");
+                }
+
+
+                // spacje
+                for (k = 0; k < (p - x) / 2; k++)
+                {
+                    Console.Write(" ");
+                }
+
+
+                // nowa linia
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadanie1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+
+    struct Osoba {
+
+        public string imie;
+        public string nazwisko;
+        public string adres;
+        public string telefon;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba os;
+
+            Console.WriteLine("Podaj imie: ");
+            os.imie = Console.ReadLine();
+
+            Console.WriteLine("Podaj nazwisko: ");
+            os.nazwisko = Console.ReadLine();
+
+            Console.WriteLine("Podaj adres: ");
+            os.adres = Console.ReadLine();
+
+            Console.WriteLine("Podaj numer telefonu");
+            os.telefon = Console.ReadLine();
+
+            for (int i = 0; i < 30; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            Console.WriteLine("* "+ os.imie + " " + os.nazwisko + " *");
+            Console.WriteLine("* " + os.adres + " *");
+            Console.WriteLine("* " + os.telefon + " *");
+            Console.WriteLine();
+            for (int i = 0; i < 30; i++)
+            {
+                Console.Write("*");
+            }
+
+
+            // naprawa wizytówki
+            // etap 1. znalezienie najdłuższego napisu - n
+            // etap 2. obliczenie n+2+2 - ilość gwiazdek w wierszu otwierającym i zamykającym
+            // etap 3. obliczenie ilość spacji w pozostałych wierszach - (n+4 - 2) / 2
+            // etap 4. zbadanie kwestii parzystości długości napisów
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
